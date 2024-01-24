618a6d009c51e052ea9b4363c6104de4ec810cf9
ialmendrasafe <almendraivan210814@gmail.com>
2024-01-24 20:27:12 -0300

    save user con password encryptado

--- auth/pkg/models/models.go
+++ auth/pkg/models/models.go
@@ -6,17 +6,17 @@ type UserLogin struct {
 }
 
 type UserSignup struct {
-	Username        string `json:username`
-	Password        string `json:password`
-	ConfirmPassword string `json:confirmPassword`
-	Email           string `json:email`
-	Phone           string `json:phone`
-	Name            string `json:name`
-	LastName        string `json:lastName`
-	Address         string `json:address`
-	City            string `json:city`
-	Country         string `json:country`
-	PostalCode      string `json:postalCode`
-	Building        string `json:building`
-	Apartment       string `json:apartment`
+	Username   string `json:username`
+	Password   string `json:password`
+	Email      string `json:email`
+	Phone      string `json:phone`
+	Name       string `json:name`
+	LastName   string `json:lastName`
+	Address    string `json:address`
+	City       string `json:city`
+	Country    string `json:country`
+	PostalCode string `json:postalCode`
+	Building   string `json:building`
+	Apartment  string `json:apartment`
+	Active     bool   `json:active`
 }
--- auth/pkg/models/mongodb/users.go
+++ auth/pkg/models/mongodb/users.go
@@ -2,19 +2,45 @@ package mongodb
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/fxivan/microservicio/auth/pkg/models"
 	"github.com/fxivan/microservicio/auth/pkg/response"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserSignupModel struct {
 	C *mongo.Collection
 }
 
+func HashBcrypt(text string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(text), 10)
+	return string(hash), err
+}
+
 func (m UserSignupModel) InsertRegisterUser(userSignup *models.UserSignup) (*response.Response, error) {
-	_, err := m.C.InsertOne(context.TODO(), userSignup)
+
+	fmt.Println("Insertando Usuario", userSignup)
+
+	encryptedPassword, err := HashBcrypt(userSignup.Password)
+
+	_, err = m.C.InsertOne(context.TODO(), bson.M{
+		"username":   userSignup.Username,
+		"password":   encryptedPassword,
+		"email":      userSignup.Email,
+		"phone":      userSignup.Phone,
+		"name":       userSignup.Name,
+		"lastName":   userSignup.LastName,
+		"address":    userSignup.Address,
+		"city":       userSignup.City,
+		"country":    userSignup.Country,
+		"postalCode": userSignup.PostalCode,
+		"building":   userSignup.Building,
+		"apartment":  userSignup.Apartment,
+		"active":     userSignup.Active,
+	})
 	if err != nil {
 		return nil, err
 	}
