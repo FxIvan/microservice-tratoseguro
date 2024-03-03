95ace8781969eaa5f53e30400d18f4616d9bfd8f
ialmendrasafe <almendraivan210814@gmail.com>
2024-03-03 11:28:23 -0300

    separando db

--- auth/cmd/app/main.go
+++ auth/cmd/app/main.go
@@ -31,7 +31,6 @@ func main() {
 	serverAddr := flag.String("serverAddr", "", "HTTP server network address")
 	serverPort := flag.Int("serverPort", 4000, "HTTP server network port")
 	mongoURI := flag.String("mongoURI", fmt.Sprintf("mongodb://%s:%s@%s:%d/%s?authSource=admin", mongo_user, mongo_password, "mongo", 27017, "user"), "Mongo Connection Uri")
-
 	mondoDatabase := flag.String("mongoDatabase", "users", "MongoDB database")
 	flag.Parse()
 
