3cf697cdfa37d12a43ef57320e7fac6e1946bd04
ialmendrasafe <almendraivan210814@gmail.com>
2024-03-03 21:05:13 -0300

    pasando variable de entorno por el build

--- agreement/cmd/app/main.go
+++ agreement/cmd/app/main.go
@@ -26,12 +26,12 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	//mongo_user := os.Getenv("MONGO_USER")
-	//mongo_password := os.Getenv("MONGO_PASSWORD")
+	mongo_user := os.Getenv("MONGO_USER")
+	mongo_password := os.Getenv("MONGO_PASSWORD")
 
 	serverAddr := flag.String("serverAddr", "", "HTTP server network address")
 	serverPort := flag.Int("serverPort", 4000, "HTTP server network port")
-	mongoURI := flag.String("mongoURI", fmt.Sprintf("mongodb://%s:%s@%s:%d/%s?authSource=admin", "admtratoseguro210814", "LkdU7ZDADARiFEtZiKJUjUeg5Swfyq9dA7qwkqjerkpQZwEvUs", "mongo", 27017, "agreement"), "Mongo Connection Uri")
+	mongoURI := flag.String("mongoURI", fmt.Sprintf("mongodb://%s:%s@%s:%d/%s?authSource=admin", mongo_user, mongo_password, "mongo", 27017, "agreement"), "Mongo Connection Uri")
 	mongoDBctpyFileName := flag.String("nameDBctpyFilename", "CounterpartFilename", "Name DB")
 	mongoDBagrDefinition := flag.String("nameDBagrDefinition", "AgreementDefinition", "Name DB")
 	flag.Parse()
