c3628308b68a22ecb4f27646e72db9b741877514
FxIvan <almendraivan210814@gmail.com>
2024-03-13 18:41:07 -0300

    agregando docker-compose

--- agreement/pkg/models/mongodb/users.go
+++ agreement/pkg/models/mongodb/users.go
@@ -12,25 +12,15 @@ type UserSignupModel struct {
 	C *mongo.Collection
 }
 
-func (m UserSignupModel) getAllUsers() ([]*models.UserSignup, error) {
-	var users []*models.UserSignup
-	cursor, err := m.C.Find(context.Background(), bson.M{})
-	if err != nil {
-		return nil, err
-	}
-	defer cursor.Close(context.Background())
+func (m UserSignupModel) searchUser(UserSignup *models.UserSignup) (string, bool) {
 
-	for cursor.Next(context.Background()) {
-		var user models.UserSignup
-		if err := cursor.Decode(&user); err != nil {
-			return nil, err
-		}
-		users = append(users, &user)
+	filter := bson.M{"username": UserSignup.Username}
+
+	findUsername := m.C.FindOne(context.Background(), filter)
+
+	if findUsername.Err() != nil {
+		return "El usuario no existe", false
 	}
 
-	if err := cursor.Err(); err != nil {
-		return nil, err
-	}
-
-	return users, nil
+	return "Usuario existe", true
 }
